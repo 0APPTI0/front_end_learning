04957212b56043a474cea02836252a9bf41770ff
0APPTI0 <2458530429@qq.com>
2020-02-03 16:50:32 +0800

    实践了删除列表项的功能

--- react16/demo01/src/Xiaojiejie.js
+++ react16/demo01/src/Xiaojiejie.js
@@ -4,8 +4,8 @@ class Xiaojiejie extends Component{
     constructor(props){
         super(props)
         this.state={
-            inputValue: "111",
-            list:[]
+            inputValue: "",
+            list:['基础按摩','精油推背']
         }
     }
 
@@ -18,11 +18,18 @@ class Xiaojiejie extends Component{
             <Fragment>
                 <div>
                     <input value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
-                    <button>增加服务</button>
+                    <button onClick={this.addList.bind(this)}>增加服务</button>
                 </div>
                 <ul>
-                    <li>头部按摩</li>
-                    <li>精油推背</li>
+                    {
+                        this.state.list.map((item,index)=>{
+                        return (
+                            <li key={index+item} onClick={this.deleteItem.bind(this,index)}>
+                                {item}
+                            </li>
+                        )
+                        })
+                    }
                 </ul>
             </Fragment>
         )
@@ -34,6 +41,31 @@ class Xiaojiejie extends Component{
         this.setState({
             inputValue: e.target.value
         })
+    }
+
+    addList()
+    {
+        this.setState({
+            // ...是扩展运算符，这里可以将inputvalue的值添加到列表之中
+            list:[...this.state.list,this.state.inputValue]
+        })
+    }
+
+    deleteItem(index)
+    {
+        //console.log(index)
+        // 复制出一个局部的list
+        let list = this.state.list
+        list.splice(index,1)
+        /* 
+            this.state.list.splice(index,1),为什么不能这么写（进行直接操作）？
+            在react后期优化的时候会产生很大的性能障碍
+            应该先声明一个局部变量，然后用局部变量对原本的变量进行赋值
+        */
+        this.setState({
+            list:list
+        })
+
     }
 }
 
