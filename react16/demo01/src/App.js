7d535ddc2978f3445f9591d615d5cbd55a6b3b46
0APPTI0 <2458530429@qq.com>
2020-02-03 16:13:54 +0800

    exp

--- react16/demo01/src/App.js
+++ react16/demo01/src/App.js
@@ -4,9 +4,10 @@ import React, {Component} from 'react'
 class App extends Component {
     render() {
       return (
-        <div>
-            Hello React!
-        </div>
+        <ul className='my-list'>
+            <li>{true?'1':'2'}</li>
+            <li>I Lov</li>
+        </ul>
       );
     }
   }
---
+++ react16/demo01/src/Xiaojiejie.js
@@ -0,0 +1,40 @@
+import React, { Component, Fragment } from 'react'
+
+class Xiaojiejie extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            inputValue: "111",
+            list:[]
+        }
+    }
+
+
+
+
+    render()
+    {
+        return(
+            <Fragment>
+                <div>
+                    <input value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
+                    <button>增加服务</button>
+                </div>
+                <ul>
+                    <li>头部按摩</li>
+                    <li>精油推背</li>
+                </ul>
+            </Fragment>
+        )
+    }
+
+    inputChange(e)
+    {
+        console.log(e.target.value)
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+}
+
+export default Xiaojiejie
