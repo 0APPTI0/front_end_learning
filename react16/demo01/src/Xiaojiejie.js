5dbc0d2c22c48681e1ce554ccddf81bb9f55445e
0APPTI0 <2458530429@qq.com>
2020-02-08 15:30:17 +0800

    push

--- react16/demo01/src/Xiaojiejie.js
+++ react16/demo01/src/Xiaojiejie.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import './style.css'
 
 class Xiaojiejie extends Component{
     constructor(props){
@@ -17,15 +18,18 @@ class Xiaojiejie extends Component{
         return(
             <Fragment>
                 <div>
-                    <input value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
+                    <label htmlFor="jspang">增加服务：</label>
+                    <input id="jspang" className="input" value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
                     <button onClick={this.addList.bind(this)}>增加服务</button>
                 </div>
                 <ul>
                     {
                         this.state.list.map((item,index)=>{
                         return (
-                            <li key={index+item} onClick={this.deleteItem.bind(this,index)}>
-                                {item}
+                            <li key={index+item} 
+                                onClick={this.deleteItem.bind(this,index)}
+                                dangerouslySetInnerHTML = {{__html:item}}
+                            >
                             </li>
                         )
                         })
